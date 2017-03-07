d1caa1312a3ce150c50f48eabc90621d9f2432f3
Miles Rains <mileskrains@gmail.com>
2017-03-07 11:28:09 -0800

    BUG: fix bug in date parsing

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -27,10 +27,10 @@ def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL, force_download=Fal
     data = pd.read_csv('Fremont.csv', index_col='Date')
 
     try:
-        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
     except TypeError:
         data.index = pd.to_datetime(data.index)
-    
+
     data.columns = ['West', 'East']
     data['Total'] = data['West'] + data['East']
     return data
